06bd6bef0d9502f2054060de38f60aeca556e4a4
diegogarcias <diego.garcia.s1996@gmail.com>
2016-09-12 22:45:35 -0300

    le cambie el titulo y agregue documento de prueba

--- ejemplo_seno.py
+++ ejemplo_seno.py
@@ -7,6 +7,6 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0,15)
 plt.plot(x, np.sin(x), 'o-')
-plt.title('Diego Garcia')
+plt.title('Diego Garcia sin tilde')
 plt.savefig("Seno")
 plt.show()
