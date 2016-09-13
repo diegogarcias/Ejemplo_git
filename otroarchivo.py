c8a857e7ee828541c2333f6a4982f12170d7f8ef
diegogarcias <diego.garcia.s1996@gmail.com>
2016-09-12 23:17:03 -0300

    agregue archivo

---
+++ otroarchivo.py
@@ -0,0 +1,12 @@
+'''
+En este codigo se plotea la funcion Seno
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,15)
+plt.plot(x, np.sin(x), 'o-')
+plt.title('Diego Garcia holi')
+plt.savefig("Seno")
+plt.show()
